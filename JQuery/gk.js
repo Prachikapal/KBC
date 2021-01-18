24c7db8186bb9d6a29905296a4bf3679ffd824e2
Prachika <prachikapal@gmail.com>
2021-01-18 17:21:45 +0530

    change question

--- JQuery/gk.js
+++ JQuery/gk.js
@@ -1,109 +1,175 @@
-$(document).ready(function(){
-    let quesArray = [".MOV extension refers usually to what kind of file",
-                    "OS computer abbreviation usually means",
-                    "Which of the following is not written by MUNSHI PREMCHAND",
-                    "Greenland is under the rule of",
-                    "Who invented BALLPOINT PEN",
-                    "Entomology is the science that studies",
-                    "Eritrea which became the 182nd member of the UN in 1993, is in the continent of",
-                    "Aam Admi Bima Yogana was launched on :",
-                    "Mahatma Gandhi laid down four preconditions for a Satyagrahi to fulfill - namely:",
-                    "Kya Nikku pagal h ?"];
-    
-    let aArray = ["Image File","Order of significance","Goban","Turkey","Biro Brothers","Behaviour of human being","Africa","November 14, 2011","Observe perfect Chastity, Adopt Poverty, Follow truth and Cultivated Fearlessness.","Yes"];
-    
-    let bArray = ["Animation/movie file","open software","Godan","Denmark","Waterman Brothers","Insects","Australia","March 5, 2009","Observe perfect Chastity, Gain Money, Follow truth and Cultivated Fearlessness.","No"];
-    
-    let cArray = ["Audio File","Operating System","Guide","Germany","Bicc Brothers","The formatio of rocks","Europe","March 10, 2008","Observe perfect Chastity, Adopt Poverty, Follow truth and Cultivated Power.","Don't Know"];
-    
-    let dArray = ["MS Office document","Optical Sensor","Manasorovar","Portugal","Write Brothers","The origin and history of technical and scientific terms","Asia","October 2, 2007","Observe perfect Chastity, Gain Money, Follow truth and Cultivated Power.","Bhot zyada pagal h"];
-    
-    let correctArray = ["Animation/movie file","Operating System","Guide","Denmark","Biro Brothers","Insects","Africa","October 2, 2007","Observe perect Chastity, Adopt Poverty, Follow truth and Cultivated Fearlessness.","Bhot zyada pagal h"];
-    
-    
-    let time = 0;
-    let globalVar,answer, i =0;
-    
-    let a = $(`<input type = "radio" id = "one" class = "option" name = "select"> &nbsp <label for = "one" id = "opt1"></label> </input><br>`);
-    $(a).appendTo("#sopt");
-    let b = $(`<input type = "radio" id = "two" class = "option" name = "select"> &nbsp <label for = "two" id = "opt2"></label> </input><br>`)
-    $(b).appendTo("#sopt");
-    let c = $(`<input type = "radio" id = "three" class = "option" name = "select"> &nbsp <label for = "three" id = "opt3"></label> </input><br>`);
-    $(c).appendTo("#sopt");
-    let d = $(`<input type = "radio" id = "four" class = "option" name = "select"> &nbsp <label for = "four" id = "opt4"></label> </input><br>`);
-    $(d).appendTo("#sopt");
+$(document).ready(function () {
+  let quesArray = [
+    ".MOV extension refers usually to what kind of file",
+    "OS computer abbreviation usually means",
+    "Which of the following is not written by MUNSHI PREMCHAND",
+    "Greenland is under the rule of",
+    "Who invented BALLPOINT PEN",
+    "Entomology is the science that studies",
+    "Eritrea which became the 182nd member of the UN in 1993, is in the continent of",
+    "Aam Admi Bima Yogana was launched on :",
+    "Mahatma Gandhi laid down four preconditions for a Satyagrahi to fulfill - namely:",
+    "Fathometer is used to measure ?",
+  ];
+
+  let aArray = [
+    "Image File",
+    "Order of significance",
+    "Goban",
+    "Turkey",
+    "Biro Brothers",
+    "Behaviour of human being",
+    "Africa",
+    "November 14, 2011",
+    "Observe perfect Chastity, Adopt Poverty, Follow truth and Cultivated Fearlessness.",
+    "Earthquakes",
+  ];
+
+  let bArray = [
+    "Animation/movie file",
+    "open software",
+    "Godan",
+    "Denmark",
+    "Waterman Brothers",
+    "Insects",
+    "Australia",
+    "March 5, 2009",
+    "Observe perfect Chastity, Gain Money, Follow truth and Cultivated Fearlessness.",
+    "Rainfall",
+  ];
+
+  let cArray = [
+    "Audio File",
+    "Operating System",
+    "Guide",
+    "Germany",
+    "Bicc Brothers",
+    "The formatio of rocks",
+    "Europe",
+    "March 10, 2008",
+    "Observe perfect Chastity, Adopt Poverty, Follow truth and Cultivated Power.",
+    "Ocean depth",
+  ];
+
+  let dArray = [
+    "MS Office document",
+    "Optical Sensor",
+    "Manasorovar",
+    "Portugal",
+    "Write Brothers",
+    "The origin and history of technical and scientific terms",
+    "Asia",
+    "October 2, 2007",
+    "Observe perfect Chastity, Gain Money, Follow truth and Cultivated Power.",
+    "Sound intensity",
+  ];
+
+  let correctArray = [
+    "Animation/movie file",
+    "Operating System",
+    "Guide",
+    "Denmark",
+    "Biro Brothers",
+    "Insects",
+    "Africa",
+    "October 2, 2007",
+    "Observe perect Chastity, Adopt Poverty, Follow truth and Cultivated Fearlessness.",
+    "Sound intensity",
+  ];
+
+  let time = 0;
+  let globalVar,
+    answer,
+    i = 0;
+
+  let a = $(
+    `<input type = "radio" id = "one" class = "option" name = "select"> &nbsp <label for = "one" id = "opt1"></label> </input><br>`
+  );
+  $(a).appendTo("#sopt");
+  let b = $(
+    `<input type = "radio" id = "two" class = "option" name = "select"> &nbsp <label for = "two" id = "opt2"></label> </input><br>`
+  );
+  $(b).appendTo("#sopt");
+  let c = $(
+    `<input type = "radio" id = "three" class = "option" name = "select"> &nbsp <label for = "three" id = "opt3"></label> </input><br>`
+  );
+  $(c).appendTo("#sopt");
+  let d = $(
+    `<input type = "radio" id = "four" class = "option" name = "select"> &nbsp <label for = "four" id = "opt4"></label> </input><br>`
+  );
+  $(d).appendTo("#sopt");
+  $("#time").text(`00 : ${time++}`);
+
+  let clock = setInterval(() => {
     $("#time").text(`00 : ${time++}`);
-    
-    let clock = setInterval(() => {
-        $("#time").text(`00 : ${time++}`);
-        if(time === 60){
-            time = 0;
-            i++;
-            if(i === 10){
-                end();
-            }
-            globalVar = requestAnimationFrame(ques);
-        }
-    },1000);
-    
-    let ques = function(){
-        setTimeout( () => { $("#score").css("border","none")}, 1000);
-        
-        $("#one").prop("checked",false);
-        $("#two").prop("checked",false);
-        $("#three").prop("checked",false);
-        $("#four").prop("checked",false);
-        $("question").text("");
-        $("#question").text(quesArray[i]);
-        $("#opt1").text("");
-        $("#opt1").text(aArray[i]);
-        $("#one").attr("value",aArray[i]);
-        $("#opt2").text("");
-        $("#opt2").text(bArray[i]);
-        $("#two").attr("value",bArray[i]);
-        $("#opt3").text("");
-        $("#opt3").text(cArray[i]);
-        $("#three").attr("value",cArray[i]);
-        $("#opt4").text("");
-        $("#opt4").text(dArray[i]);
-        $("#four").attr("value",dArray[i]); 
+    if (time === 60) {
+      time = 0;
+      i++;
+      if (i === 10) {
+        end();
+      }
+      globalVar = requestAnimationFrame(ques);
     }
-    
+  }, 1000);
+
+  let ques = function () {
+    setTimeout(() => {
+      $("#score").css("border", "none");
+    }, 1000);
+
+    $("#one").prop("checked", false);
+    $("#two").prop("checked", false);
+    $("#three").prop("checked", false);
+    $("#four").prop("checked", false);
+    $("question").text("");
+    $("#question").text(quesArray[i]);
+    $("#opt1").text("");
+    $("#opt1").text(aArray[i]);
+    $("#one").attr("value", aArray[i]);
+    $("#opt2").text("");
+    $("#opt2").text(bArray[i]);
+    $("#two").attr("value", bArray[i]);
+    $("#opt3").text("");
+    $("#opt3").text(cArray[i]);
+    $("#three").attr("value", cArray[i]);
+    $("#opt4").text("");
+    $("#opt4").text(dArray[i]);
+    $("#four").attr("value", dArray[i]);
+  };
+
+  globalVar = requestAnimationFrame(ques);
+
+  let button = $(`<button id = "btn">NEXT</button>`);
+  $(button).appendTo("#frame");
+
+  button.click(() => {
+    let demo = $(".option");
+    for (const j of demo) {
+      if (j.checked) {
+        answer = j.value;
+        break;
+      }
+    }
+
+    if (answer === correctArray[i]) {
+      $("#span").text(parseInt($("#span").text()) + 5);
+    } else {
+      $("#score").css("border", "5px solid red");
+    }
+
+    time = 0;
+    i++;
+    if (i === 10) {
+      end();
+    }
     globalVar = requestAnimationFrame(ques);
-    
-    let button = $(`<button id = "btn">NEXT</button>`);
-    $(button).appendTo("#frame");
-    
-    button.click( () => {
-        
-        let demo = $(".option");
-        for(const j of demo){
-            if(j.checked){
-                answer = j.value;
-                break;
-            }
-        }
-    
-        if(answer === correctArray[i]){
-            $("#span").text(parseInt($("#span").text()) + 5);
-        }
-        else{
-            $("#score").css("border","5px solid red");
-        }
-       
-    
-        time = 0;
-        i++;
-        if (i === 10){
-            end();
-        }
-        globalVar = requestAnimationFrame(ques);
-    });
-    
-    function end(){
-        cancelAnimationFrame(globalVar);
-        clearInterval(clock);
-        setTimeout( () => {  alert(`Your Score is : ${$("#span").text()}`);} , 1000);
-    }
-    
-    });
+  });
+
+  function end() {
+    cancelAnimationFrame(globalVar);
+    clearInterval(clock);
+    setTimeout(() => {
+      alert(`Your Score is : ${$("#span").text()} out 0f 50`);
+    }, 1000);
+  }
+});
--- JQuery/science.js
+++ JQuery/science.js
@@ -1,116 +1,169 @@
 $(document).ready(() => {
-    let queArray = ["Which animal never drinks water in its entire life",
-        "which is the vertebrate that has two chambered heart",
-        "There are _____ number of muscles in human",
-        "What is the life span of RBC",
-        "The number of ribs in human body is _____",
-        "Which is the smallest flightless bird ?",
-        "Hormones are produced by _____",
-        "Which of the following is the MASTER GLAND",
-        "What is the normal value of blood sugar in the body ?",
-        "Which of the following carries impure blood ?"];
+  let queArray = [
+    "Which animal never drinks water in its entire life",
+    "which is the vertebrate that has two chambered heart",
+    "There are _____ number of muscles in human",
+    "What is the life span of RBC",
+    "The number of ribs in human body is _____",
+    "Which is the smallest flightless bird ?",
+    "Hormones are produced by _____",
+    "Which of the following is the MASTER GLAND",
+    "What is the normal value of blood sugar in the body ?",
+    "Which of the following carries impure blood ?",
+  ];
 
-    let aArray = ["Kangaroo", "Fish", "638", "130 days", "23", "Kiwi", "Endocrine glands", "Thymus gland",
-        "80 to 120mg/100 ml of blood", "Pulmonary artery"];
+  let aArray = [
+    "Kangaroo",
+    "Fish",
+    "638",
+    "130 days",
+    "23",
+    "Kiwi",
+    "Endocrine glands",
+    "Thymus gland",
+    "80 to 120mg/100 ml of blood",
+    "Pulmonary artery",
+  ];
 
-    let bArray = ["Hippopotamus", "Snake", "637", "110 days", "24", "Penguin", "Pituitary glands", "Pancreas",
-        "70 to 120mg/100 ml of blood", "Pulmonary vein"];
+  let bArray = [
+    "Hippopotamus",
+    "Snake",
+    "637",
+    "110 days",
+    "24",
+    "Penguin",
+    "Pituitary glands",
+    "Pancreas",
+    "70 to 120mg/100 ml of blood",
+    "Pulmonary vein",
+  ];
 
-    let cArray = ["Rat", "Blue Whale", "639", "100 days", "25", "Ostrich", "Hypothalamus", "Pineal gland",
-        "90 to 120mg/100 ml of blood", "Alveoli"];
+  let cArray = [
+    "Rat",
+    "Blue Whale",
+    "639",
+    "100 days",
+    "25",
+    "Ostrich",
+    "Hypothalamus",
+    "Pineal gland",
+    "90 to 120mg/100 ml of blood",
+    "Alveoli",
+  ];
 
-    let dArray = ["Kangaroo_rat", "Crocodile", "640", "120 days", "22", "Rhea", "Pancreas", "Pituitary gland",
-        "60 to 120mg/100 ml of blood", "Aorta"];
+  let dArray = [
+    "Kangaroo_rat",
+    "Crocodile",
+    "640",
+    "120 days",
+    "22",
+    "Rhea",
+    "Pancreas",
+    "Pituitary gland",
+    "60 to 120mg/100 ml of blood",
+    "Aorta",
+  ];
 
-    let correctArray = ["Kangaroo_rat","Fish","639","120 days","24","Kiwi","Endocrine glands","Pituitary gland",
-                        "80 to 120mg/100 ml of blood","Pulmonary artery"];
+  let correctArray = [
+    "Kangaroo_rat",
+    "Fish",
+    "639",
+    "120 days",
+    "24",
+    "Kiwi",
+    "Endocrine glands",
+    "Pituitary gland",
+    "80 to 120mg/100 ml of blood",
+    "Pulmonary artery",
+  ];
 
-    let time = 00;
-    let i = 0;
-    let opt1, opt2, opt3, opt4;
-    let globalVar;
+  let time = 00;
+  let i = 0;
+  let opt1, opt2, opt3, opt4;
+  let globalVar;
 
+  $("#time").text(`00 : ${time}`);
+  let clock = setInterval(() => {
     $("#time").text(`00 : ${time}`);
-    let clock = setInterval(() => {
-        $("#time").text(`00 : ${time}`);
-        time++;
-        if (time === 60) {
-            time = 00;
-            i++;
-            globalVar = requestAnimationFrame(ques);
-        }
-    }, 1000);
+    time++;
+    if (time === 60) {
+      time = 00;
+      i++;
+      globalVar = requestAnimationFrame(ques);
+    }
+  }, 1000);
 
-    
+  $("#question").text(queArray[i]);
+  opt1 = $(
+    `<input type = "radio" name = "first" id = "one" class = "option">&nbsp<label for = "one" id = "opt1"></label><br>`
+  );
+  $(opt1).appendTo("#sopt");
+  opt2 = $(
+    `<input type = "radio" name = "first" id = "two" class = "option">&nbsp<label for = "two" id = "opt2"></label><br>`
+  );
+  $(opt2).appendTo("#sopt");
+  opt3 = $(
+    `<input type = "radio" name = "first" id = "three" class = "option">&nbsp<label for = "three" id = "opt3"></label><br>`
+  );
+  $(opt3).appendTo("#sopt");
+  opt4 = $(
+    `<input type = "radio" name = "first" id = "four" class = "option">&nbsp<label for = "four" id = "opt4"></label><br>`
+  );
+  $(opt4).appendTo("#sopt");
 
+  let button = $(`<button id = "btn">NEXT</button>`);
+  $(button).appendTo("#frame");
 
+  function ques() {
+    $("#one").prop("checked", false);
+    $("#two").prop("checked", false);
+    $("#three").prop("checked", false);
+    $("#four").prop("checked", false);
+    $("#question").text("");
     $("#question").text(queArray[i]);
-    opt1 = $(`<input type = "radio" name = "first" id = "one" class = "option">&nbsp<label for = "one" id = "opt1"></label><br>`);
-    $(opt1).appendTo("#sopt");
-    opt2 = $(`<input type = "radio" name = "first" id = "two" class = "option">&nbsp<label for = "two" id = "opt2"></label><br>`);
-    $(opt2).appendTo("#sopt");
-    opt3 = $(`<input type = "radio" name = "first" id = "three" class = "option">&nbsp<label for = "three" id = "opt3"></label><br>`);
-    $(opt3).appendTo("#sopt");
-    opt4 = $(`<input type = "radio" name = "first" id = "four" class = "option">&nbsp<label for = "four" id = "opt4"></label><br>`);
-    $(opt4).appendTo("#sopt");
+    $("#opt1").text("");
+    $("#opt1").text(aArray[i]);
+    $("#one").attr("value", aArray[i]);
+    $("#opt2").text("");
+    $("#opt2").text(bArray[i]);
+    $("#two").attr("value", bArray[i]);
+    $("#opt3").text("");
+    $("#opt3").text(cArray[i]);
+    $("#three").attr("value", cArray[i]);
+    $("#opt4").text("");
+    $("#opt4").text(dArray[i]);
+    $("#four").attr("value", dArray[i]);
+    if (i === 10) {
+      end();
+      cancelAnimationFrame(globalVar);
+    }
+  }
 
-    let button = $(`<button id = "btn">NEXT</button>`);
-    $(button).appendTo("#frame");
+  globalVar = requestAnimationFrame(ques);
 
-        
-
-    function ques(){
-        $("#one").prop("checked",false);
-        $("#two").prop("checked",false);
-        $("#three").prop("checked",false);
-        $("#four").prop("checked",false);
-        $("#question").text("");
-        $("#question").text(queArray[i]);
-        $("#opt1").text("");  
-        $("#opt1").text(aArray[i]);  
-        $("#one").attr("value",aArray[i]);
-        $("#opt2").text("");
-        $("#opt2").text(bArray[i]);
-        $("#two").attr("value",bArray[i]);
-        $("#opt3").text("");
-        $("#opt3").text(cArray[i]);
-        $("#three").attr("value",cArray[i]);
-        $("#opt4").text("");
-        $("#opt4").text(dArray[i]);
-        $("#four").attr("value",dArray[i]);
-        if (i === 10) {
-            end();
-            cancelAnimationFrame(globalVar);
-        }
+  $("#btn").click(() => {
+    let demo = $(".option");
+    let selectedValue;
+    for (const rb of demo) {
+      if (rb.checked) {
+        selectedValue = rb.value;
+        break;
+      }
     }
 
-    globalVar = requestAnimationFrame(ques)
+    if (selectedValue === correctArray[i]) {
+      let score = $("#span").text();
+      score = parseInt(score) + 5;
+      $(span).text(score);
+    }
 
-   $("#btn").click(()=>{
-       let demo = $(".option");
-       let selectedValue;
-       for(const rb of demo){
-           if(rb.checked){
-               selectedValue = rb.value;
-               break;
-           }
-       }
-       
-       if(selectedValue === correctArray[i]){  
-           let score = $("#span").text();  
-           score = parseInt(score) + 5;
-           $(span).text(score);
-       }
+    i++;
+    time = 0;
+    globalVar = requestAnimationFrame(ques);
+  });
 
-       i++;
-       time = 0;
-       globalVar = requestAnimationFrame(ques);
-   });
-
-
-   function end(){
-      alert("Your Score is  : " + $("#span").text());
-      clearInterval(clock);
-   }
-
+  function end() {
+    alert("Your Score is  : " + $("#span").text() + " out of 50");
+    clearInterval(clock);
+  }
 });
