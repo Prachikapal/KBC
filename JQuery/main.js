30e85de1d397a541604c30609d3c335f424c8397
Prachika <prachikapal@gmail.com>
2021-01-18 16:57:50 +0530

    change location in main.js file

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,37 +1,28 @@
-$(document).ready( function(){
-    let radioSelect;
-    let op;
+$(document).ready(function () {
+  let radioSelect;
+  let op;
 
-    $("#btn").click(function(){
-        let demo = $(".space");
-        for(let j of demo){
-            if(j.checked){
-                radioSelect = j.id;
-                break;
-            }
-        }
-      
+  $("#btn").click(function () {
+    let demo = $(".space");
+    for (let j of demo) {
+      if (j.checked) {
+        radioSelect = j.id;
+        break;
+      }
+    }
 
-        if(radioSelect === "science"){
-            window.location.href = "../HTML/science.html";
-        }
-        else if(radioSelect === "cartoon"){
-            window.location.href = "../HTML/cartoon.html";
-        }
-        else if(radioSelect === "gk"){
-            window.location.href = "../HTML/gk.html";
-        }
-        else if(radioSelect === "sports"){
-            window.location.href = "../HTML/sports.html";
-        }
-        else if(radioSelect === "bollywood"){
-            window.location.href = "../HTML/bollywood.html";
-        }
-        else if(radioSelect === "politics"){
-            window.location.href = "../HTML/politics.html";
-        }
-
-
-    });
-    
+    if (radioSelect === "science") {
+      window.location.href = "./HTML/science.html";
+    } else if (radioSelect === "cartoon") {
+      window.location.href = "./HTML/cartoon.html";
+    } else if (radioSelect === "gk") {
+      window.location.href = "./HTML/gk.html";
+    } else if (radioSelect === "sports") {
+      window.location.href = "./HTML/sports.html";
+    } else if (radioSelect === "bollywood") {
+      window.location.href = "./HTML/bollywood.html";
+    } else if (radioSelect === "politics") {
+      window.location.href = "./HTML/politics.html";
+    }
+  });
 });
